1786f0a24da8ce3822d8ecbc82a46a9d59b96470
Junyi Sun <sunjunyiwd@gmail.com>
2014-11-13 15:59:20 -0500

    Nov 3
    add top-down dp solution

--- Nov 3/Palindrome Partitioning II.java
+++ Nov 3/Palindrome Partitioning II.java
@@ -47,3 +47,54 @@ public class Solution {
         return true;
     }
 }
+
+
+//Top-Down DP Solution : Recursion + Memo
+//Perform much much better but still Time Limit Exceeded
+public class Solution {
+    
+    private Map<String, Integer> memo = new HashMap<String, Integer>();
+    
+    public int minCut(String s) {
+        
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        
+        return helper(s);
+    }
+    
+    private int helper(String s) {
+        if (memo.containsKey(s)) {
+            return memo.get(s);
+        }
+        
+        int length = s.length();
+        for (int i = 1; i <= length; i++) {
+            String prefix = s.substring(0, i);
+            if (isValid(prefix)) {
+                if (i == length) {
+                    memo.put(s, 1);
+                } else {
+                    int min = helper(s.substring(i));
+                    memo.put(s, min + 1);
+                }
+            }
+        }
+        
+        return memo.get(s);
+    }
+    
+    private boolean isValid(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}

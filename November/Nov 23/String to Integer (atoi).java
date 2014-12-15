f2a418bdd760ec971dfb01a6ebc747c94cf7cadb
Junyi Sun <sunjunyiwd@gmail.com>
2014-12-15 16:39:54 -0500

    minor modification

--- November/Nov 23/String to Integer (atoi).java
+++ November/Nov 23/String to Integer (atoi).java
@@ -1,6 +1,6 @@
 public class Solution {
     
-    private static final int max = Integer.MAX_VALUE / 10;
+    private static final int MAX = Integer.MAX_VALUE / 10;
     
     public int atoi(String str) {
         if (str == null || str.length() == 0) {
@@ -26,7 +26,7 @@ public class Solution {
         
         int result = 0;
         while (curr < str.length() && Character.isDigit(str.charAt(curr))) {
-            if (result > max || result == max && (str.charAt(curr) - '0') >= 8) {
+            if (result > MAX || result == MAX && (str.charAt(curr) - '0') >= 8) {
                 return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
             result = result * 10 + (str.charAt(curr) - '0');
